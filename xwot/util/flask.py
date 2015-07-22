35bfdf93707309f12131e4d0721b9f2181fd0db1
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-07-22 16:14:37 +0200

    clean up xwot.util.flask and add cors decorator

--- xwot/util/flask.py
+++ xwot/util/flask.py
@@ -6,56 +6,13 @@
 """
 
 from __future__ import absolute_import
-from functools import wraps
 
-from flask import request
 from flask import Response
+from datetime import timedelta
+from flask import request, current_app
+from functools import update_wrapper
 
-
-def _add_response_header(headers):
-    from flask import make_response
-
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            resp = make_response(f(*args, **kwargs))
-            h = resp.headers
-            for header, value in headers.items():
-                h[header] = value
-            return resp
-
-        return decorated_function
-
-    return decorator
-
-
-def hydra_link(apidoc_url):
-    link_header = '<' + apidoc_url + '>; rel="http://www.w3.org/ns/hydra/core#apiDocumentation"'
-
-    def wrapped_link(f):
-        @wraps(f)
-        @_add_response_header({'Link': link_header})
-        def decorated_function(*args, **kwargs):
-            return f(*args, **kwargs)
-
-        return decorated_function
-
-    return wrapped_link
-
-
-def mount_apidoc(app, builder):
-    doc = builder.documentation
-    link = hydra_link(doc.apidoc_url)
-    jsonld_doc = builder.build()
-
-    @link
-    def vocab():
-        if jsonld_doc is not None:
-            return Response(response=jsonld_doc, status=200, content_type='application/ld+json')
-        else:
-            return Response(status=404)
-
-    app.add_url_rule('/vocab', 'vocab', vocab)
+__all__ = ['cors', 'make_response']
 
 
 SERIALIZERS = {
@@ -64,6 +21,38 @@ SERIALIZERS = {
     'text/plain': lambda obj: obj.to_html(),
     'application/ld+json': lambda obj: obj.to_jsonld()
 }
+
+
+def cors(origin='*', methods=None, headers=None, max_age=2520):
+    # source: http://flask.pocoo.org/snippets/56/
+
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    else:
+        methods = 'GET, OPTIONS'
+
+    if headers is not None:
+        headers = ', '.join(headers)
+    else:
+        headers = 'x-prototype-version,x-requested-with'
+
+    from flask import make_response
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            resp = make_response(f(*args, **kwargs))
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = methods
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Headers'] = headers
+
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
 def make_response(obj, default='application/ld+json', status=200):
@@ -85,4 +74,3 @@ def make_response(obj, default='application/ld+json', status=200):
         fun_serializer = SERIALIZERS[default]
         doc = fun_serializer(obj)
         return Response(response=doc, status=status, content_type=default)
-
