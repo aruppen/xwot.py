d28028eae3aab6f1e3e809dcc65214b9f1f0c721
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-08-26 21:48:17 +0200

    add LICENSE.txt and update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# xwot.py - Python tools for the extended Web of Things
+# Copyright (C) 2015  Alexander Rüedlinger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from setuptools import setup, find_packages
 
@@ -11,8 +27,7 @@ setup(
     entry_points={
         'console_scripts': [
             'xwotc = xwot.cmd:xwotc',
-            'xwotd = xwot.cmd:xwotd',
-#            'xwotp2v = xwot.cmd:xwotp2v'
+            'xwotd = xwot.cmd:xwotd'
         ]
     },
     version=VERSION,
